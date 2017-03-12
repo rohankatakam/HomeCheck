c44542c4a2222ddff9e6427324c36e8b620221c2
Rohan Katakam <student@rkatakam.local>
2017-03-12 15:02:55 -0500

    Initial Commit

---
+++ HomeCheck/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HomeCheck
+//
+//  Created by Student on 3/12/17.
+//  Copyright © 2017 Rohan Katakam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
